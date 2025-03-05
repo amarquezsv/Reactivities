0233d82d19fc2225ed944c8b0410df43def9d285
Alfredo Marquez <55933165+amarquezsv@users.noreply.github.com>
2025-03-05 07:27:08 -0500

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,7 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
-
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 var app = builder.Build();
 
 // Para que se pueda acceder a la API desde un cliente en otro dominio
